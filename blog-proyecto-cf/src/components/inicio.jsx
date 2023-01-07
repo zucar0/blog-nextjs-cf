58115dc6e3018218876f55ed2efb462f61aa65fe
Antonio Enríquez <42826452+zucar0@users.noreply.github.com>
2023-01-07 09:42:06 -0600

    imagen main

--- blog-proyecto-cf/src/components/inicio.jsx
+++ blog-proyecto-cf/src/components/inicio.jsx
@@ -5,9 +5,12 @@ import { ListadoTrabajos } from './listado';
 export const Inicio = () => {
   return (
     <div className='home'>
-      <h2 className='main'>
-        Hola, soy <strong>Antonio Enríquez</strong>, Ing. en Desarrollo de Software, ex periodista (Lic. en Ciencias de la Comunicación) y un profesional de las Tecnologías de la Información apegado al principio de adquirir aprendizaje todos los días.
-      </h2>
+      <div className="container-header">
+        <h2 className='main'>
+          Hola, soy <strong>Antonio Enríquez</strong>, Ing. en Desarrollo de Software, ex periodista (Lic. en Ciencias de la Comunicación) y un profesional de las Tecnologías de la Información apegado al principio de adquirir aprendizaje todos los días.
+        </h2>
+        <img src={"/images/avatar.png"} alt="Antonio Enríquez" title='Antonio Enríquez | Desarrollador web' />
+      </div>
       <p className='secondary'>
         Mi <strong>perfil híbrido</strong> es un aliciente con el que se puede obtener valor agregado en cualquier trabajo. <Link to="/contacto">Contáctame</Link>
       </p>
